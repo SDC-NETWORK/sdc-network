8ea9ef4a8fe04488cdab89214e6208f069135320
Nahid Hossain <nahid4510@gmail.com>
2024-10-14 00:00:34 +0600

    upload

---
+++ main.js
@@ -0,0 +1,49 @@
+import './style.css'
+import logo from '/SDC Logo.png'
+
+
+const title = document.querySelector('title')
+title.innerText = 'SDC - Database'
+const header = document.querySelector('header')
+
+/**htmL*/
+
+header.innerHTML = `  <div class="contact-details">
+        <a href="tel:+8801728171519"><i class="fa-solid fa-phone"></i></a>
+        <a href="mailto:sdc.contact24@gmail.com"><i class="fa-solid fa-envelope"></i></a>
+        <a href="http://www.oursdc.com"><i class="fa-solid fa-globe"></i></a>
+    </div>
+    <div class="logo">
+        <img src="${logo}" onclick="location.href = 'http://localhost:5174/'" alt="Not responding">
+        <p>সাপ্লাই এন্ড ডিস্ট্রিবিউশন কর্পোরেশন </p>
+    </div>
+    <div class="social-icon">
+        <a href="#"><i class="fa-brands fa-facebook"></i></a>
+        <a href="https://wa.me/01728171519"><i class="fa-brands fa-whatsapp"></i></a>
+        <a href="https://t.me/Rofikul5858"><i class="fa-brands fa-telegram"></i></a>
+    </div>`
+
+const personPageTitle = document.querySelector('.person-page-title') 
+personPageTitle.innerHTML = `
+               <h2 class="person-title">প্রতিনিধি সমূহঃ-</h2>
+` 
+document.querySelector('.person-title').style.textAlign = 'center'
+const allLi = document.querySelectorAll('.modal')
+const x = document.querySelectorAll('.fa-x')
+const modal = document.querySelector('#modal')
+
+allLi.forEach(function(li){
+
+   li.addEventListener('click', function(){
+
+      modal.classList.add('modal-open')
+   })
+})
+
+
+
+x.forEach(function(x){
+   x.addEventListener('click', function(){
+      modal.classList.remove('modal-open')
+   });
+})
---
+++ vite.config.js
@@ -0,0 +1,77 @@
+import { defineConfig } from 'vite'
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [],
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+
+        // Meherpur
+        meherpur: 'meherpur/meherpur.html',
+
+        amdah: 'meherpur/amdah/amdah.html',
+        amdahp: 'meherpur/amdah/person.html',
+
+        amjhupi: 'meherpur/amjhupi/amjhupi.html',
+        amjhupip: 'meherpur/amjhupi/person.html',
+
+        buripota: 'meherpur/buripota/buripota.html',
+        buripotap: 'meherpur/buripota/person.html',
+
+        kutubpur: 'meherpur/kutubpur/kutubpur.html',
+        kutubpurp: 'meherpur/kutubpur/person.html',
+
+        pirojpur: 'meherpur/pirojpur/pirojpur.html',
+        pirojpurp: 'meherpur/pirojpur/person.html',
+        
+        // Gangni
+        gangni: 'gangni/gangni.html',
+
+        bamondi: 'gangni/bamondi/bamondi.html',
+        bamondip: 'gangni/bamondi/person.html',
+
+        dhankhola: 'gangni/dhankhola/dhankhola.html',
+        dhankholap: 'gangni/dhankhola/person.html',
+
+        kathuli: 'gangni/kathuli/kathuli.html',
+        kathulip: 'gangni/kathuli/person.html',
+
+        kazipur: 'gangni/kazipur/kazipur.html',
+        kazipurp: 'gangni/kazipur/person.html',
+
+        motormura: 'gangni/motormura/motormura.html',
+        motormurap: 'gangni/motormura/person.html',
+
+        raipur: 'gangni/raipur/raipur.html',
+        raipurp: 'gangni/raipur/person.html',
+        
+        saharbati: 'gangni/saharbati/saharbati.html',
+        saharbatip: 'gangni/saharbati/person.html',
+
+        sholotaka: 'gangni/sholotaka/sholotaka.html',
+        sholotakap: 'gangni/sholotaka/person.html',
+
+        tetulbaria: 'gangni/tetulbaria/tetulbaria.html',
+        tetulbariap: 'gangni/tetulbaria/person.html',
+
+        // Mujibnagar
+        mujibnagar: 'mujibnagar/mujibnagar.html',
+
+        bagoan: 'mujibnagar/bagoan/bagoan.html',
+        bagoanp: 'mujibnagar/bagoan/person.html',
+
+        dariapur: 'mujibnagar/dariapur/dariapur.html',
+        dariapurp: 'mujibnagar/dariapur/person.html',
+
+        mohajonpur: 'mujibnagar/mohajonpur/mohajonpur.html',
+        mohajonpurp: 'mujibnagar/mohajonpur/person.html',
+
+        monakhali: 'mujibnagar/monakhali/monakhali.html',
+        monakhalip: 'mujibnagar/monakhali/person.html',
+
+      },
+    },
+  },
+})
+
